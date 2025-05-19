fc02a9e63788438bc79c1c7d1ab88b65fdc5b2d5
Roni bhakta <77425964+ronibhakta1@users.noreply.github.com>
2025-05-19 18:10:42 +0530

    model fix

--- lenny/models/items.py
+++ lenny/models/items.py
@@ -12,7 +12,7 @@ class Item(Base):
     language = Column(String(30), nullable=False)
     is_readable = Column(Boolean, default=True, nullable=False)
     is_lendable = Column(Boolean, default= True, nallable= False)
-    is_wishlistable = Column(Boolean, default= True, nullable=False)
+    is_waitlistable = Column(Boolean, default= True, nullable=False)
     is_Printdisabled = Column(Boolean, default=False, nullable= False)
     num_lendable_total = Column(Integer, nullable=False) 
     current_num_lendable = Column(Integer, nullable=False)
