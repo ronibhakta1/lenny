4ad1257fb4fff8c9b942261b82d60889c1e0e3fa
Mek <michael.karpeles@gmail.com>
2025-05-20 07:58:13 -0700

    simplifies table schema for initial prototype

--- lenny/models/items.py
+++ lenny/models/items.py
@@ -6,17 +6,8 @@ from . import Base
 class Item(Base):
     __tablename__ = 'items'
     
-    lenny_edition = Column(String(100),primary_key=True)
-    title = Column(String(255), nullable=False)
-    item_status = Column(Boolean, default=True)
-    language = Column(String(30), nullable=False)
-    is_readable = Column(Boolean, default=True, nullable=False)
-    is_lendable = Column(Boolean, default= True, nallable= False)
-    is_waitlistable = Column(Boolean, default= True, nullable=False)
-    is_Printdisabled = Column(Boolean, default=False, nullable= False)
-    num_lendable_total = Column(Integer, nullable=False) 
-    current_num_lendable = Column(Integer, nullable=False)
-    current_waitlist_size =Column(Integer, default= 0, nullable=False)
+    id = Column(Integer, primary_key=True)
+    openlibrary_edition = Column(BigInteger, nullable=False)
     encrypted = Column(Boolean, default= False, nullable=False)
     s3_filepath = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), default=func.now())
