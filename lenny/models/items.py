6deeeb40c05206ea731072f0b73c261f9287f76e
Roni bhakta <77425964+ronibhakta1@users.noreply.github.com>
2025-05-20 20:29:17 +0530

    Items schema fields (#21)
    * Refactor Item model to include core fields
    
    ---------
    
    Co-authored-by: Mek <michael.karpeles@gmail.com>

--- lenny/models/items.py
+++ lenny/models/items.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python 
-from pydantic import BaseModel
+from sqlalchemy  import Column, String, Boolean, Integer, BigInteger, DateTime
+from sqlalchemy.sql import func
+from . import Base
 
-class ItemSettings(BaseModel):
-    is_lendable: bool
-    num_lendable_total: int
+class Item(Base):
+    __tablename__ = 'items'
+    
+    id = Column(Integer, primary_key=True)
+    openlibrary_edition = Column(BigInteger, nullable=False)
+    encrypted = Column(Boolean, default= False, nullable=False)
+    s3_filepath = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    
