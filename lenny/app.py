71ce458c8013a041b5a7d8a937bf2acdde39d9a3
roni bhakta <ronibhakta1@gmail.com>
2026-02-16 22:49:38 +0530

    feat: Implement OAuth 2.0 PKCE authorization flow with new models, services, and API routes, alongside database configuration and timezone adjustments.

--- lenny/app.py
+++ lenny/app.py
@@ -4,15 +4,25 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from lenny.routes import api
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
+from lenny.routes import api, oauth
 from lenny.configs import OPTIONS
 from lenny import __version__ as VERSION
+from lenny.core.limiter import limiter
 
 app = FastAPI(
     title="Lenny API",
     description="Lenny: A Free, Open Source Lending System for Libraries",
     version=VERSION,
 )
+
+# Rate Limiter
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 # App-level CORS should be a setting, as to not
 # conflict with CORS handled by nginx
@@ -27,6 +37,7 @@ app.add_middleware(
 app.templates = Jinja2Templates(directory="lenny/templates")
 
 app.include_router(api.router, prefix="/v1/api")
+app.include_router(oauth.router, prefix="/v1/oauth")
 
 app.mount("/static", StaticFiles(directory="lenny/static"), name="static")
 
